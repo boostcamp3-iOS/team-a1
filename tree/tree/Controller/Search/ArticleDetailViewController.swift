f4eaf08ba8e089785833f102415e7919d4a4acc3
kimhyeri <hyer1k@naver.com>
2019-01-22 17:49:41 +0900

    Feat : Add gesturerecognizer delegate

---
+++ tree/tree/Controller/Search/ArticleDetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ArticleDetailViewController.swift
+//  tree
+//
+//  Created by hyerikim on 22/01/2019.
+//  Copyright © 2019 gardener. All rights reserved.
+//
+
+import UIKit
+
+class ArticleDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
+extension ArticleDetailViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+}
