2c717ebeaceb65f76767cac4374218518b9f30e6
kimhyeri <hyer1k@naver.com>
2019-01-25 15:37:06 +0900

    Feat : Set stackview layout margin

--- tree/tree/Controller/Search/ArticleDetailViewController.swift
+++ tree/tree/Controller/Search/ArticleDetailViewController.swift
@@ -10,7 +10,11 @@ import UIKit
 
 class ArticleDetailViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var contentLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
