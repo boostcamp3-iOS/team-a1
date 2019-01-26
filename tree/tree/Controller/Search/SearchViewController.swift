65bb2d7c55444d32b82c2c7ed23dd9eebe54e855
onemoon <onemoonstudio@gmail.com>
2019-01-26 13:21:15 +0900

    Feat : SearchBar animation & Delegate Function
    Feat
    SearchBar animation when scroll up or down
    And Delegate Function

--- tree/tree/Controller/Search/SearchViewController.swift
+++ tree/tree/Controller/Search/SearchViewController.swift
@@ -10,13 +10,17 @@ import UIKit
 
 class SearchViewController: UIViewController {
     
+    @IBOutlet weak var uiSearchBarOuterView: UIView!
     @IBOutlet weak var uiSearchBar: UISearchBar!
     @IBOutlet weak var uiTableView: UITableView!
     @IBOutlet weak var navigationFilterItem: UIBarItem!
     
     private let cellIdentifier: String = "ArticleTableViewCell"
-    
-    private var lastContentOffset: CGFloat = -64
+    private var topOffset: CGFloat = UIApplication.shared.statusBarOrientation.isLandscape ? 44 : 64
+    private var tableViewContentOffsetY: CGFloat = 0
+    private var tableViewScrollCount: (down: Int, up: Int) = (0, 0)
+    private var searchBarTextField: UITextField?
+    private var searchBarPresented: Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,7 +44,7 @@ class SearchViewController: UIViewController {
     }
     
     func tableViewSetting() {
-        uiTableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
+        uiTableView.contentInset = UIEdgeInsets(top: topOffset, left: 0, bottom: 0, right: 0)
         uiTableView.separatorStyle = .none
     }
     
@@ -65,13 +69,65 @@ extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
         return cell
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let storyboard = UIStoryboard(name: "ArticleDetail", bundle: nil)
-        let articleView = storyboard.instantiateViewController(withIdentifier: "ArticleDetailViewController")
-        self.navigationController?.pushViewController(articleView, animated: true)
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if tableViewContentOffsetY < scrollView.contentOffset.y {
+            tableViewScrollCount.down += 1
+            tableViewScrollCount.up = 0
+            if tableViewScrollCount.down > 15 {
+                scrollSettingFunction(.down)
+            }
+        } else {
+            tableViewScrollCount.down = 0
+            tableViewScrollCount.up += 1
+            if tableViewScrollCount.up > 15 {
+                scrollSettingFunction(.up)
+            }
+        }
+        tableViewContentOffsetY = scrollView.contentOffset.y
+    }
+    
+    func scrollSettingFunction(_ direction: ScrollDirection) {
+        switch direction {
+        case .up where !searchBarPresented:
+            searchBarPresented = true
+            UIView.animate(withDuration: 0.5) { [weak self] in
+                guard let self = self else { return }
+                self.uiSearchBarOuterView.center.y += self.topOffset
+                self.uiTableView.contentInset.top = self.topOffset
+                self.uiSearchBarOuterView.alpha = 1.0
+            }
+        case .down where searchBarPresented:
+            searchBarPresented = false
+            UIView.animate(withDuration: 0.5) { [weak self] in
+                guard let self = self else { return }
+                self.uiSearchBarOuterView.center.y -= self.topOffset
+                self.uiTableView.contentInset.top = 0
+                self.uiSearchBarOuterView.alpha = 0
+            }
+        default:
+            break
+        }
+        tableViewScrollCount = (0,0)
     }
 }
 
 extension SearchViewController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        uiSearchBar.setShowsCancelButton(true, animated: true)
+    }
     
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.navigationItem.title = searchBar.text!
+        searchBarHideAndSetting()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBarTextField?.text = ""
+        searchBarHideAndSetting()
+    }
+    
+    func searchBarHideAndSetting() {
+        uiSearchBar.setShowsCancelButton(false, animated: true)
+        uiSearchBar.resignFirstResponder()
+    }
 }
---
+++ tree/tree/Helper/SearchScrollEnum.swift
@@ -0,0 +1,12 @@
+//
+//  SearchScrollEnum.swift
+//  tree
+//
+//  Created by Hyeontae on 26/01/2019.
+//  Copyright © 2019 gardener. All rights reserved.
+//
+
+enum ScrollDirection {
+    case up
+    case down
+}
