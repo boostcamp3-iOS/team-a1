26f496a72a158a9a611ab880c1f7e212cd9a6a13
onemoon <onemoonstudio@gmail.com>
2019-01-28 17:22:35 +0900

    Feat : Transition Management
    Feat
    Transition manager & controller
    feat with SearchViewController

---
+++ tree/tree/Helper/PresentationManager/PresentationController.swift
@@ -0,0 +1,81 @@
+//
+//  PresentationController.swift
+//  tree
+//
+//  Created by Hyeontae on 28/01/2019.
+//  Copyright © 2019 gardener. All rights reserved.
+//
+
+import UIKit
+
+class PresentationController: UIPresentationController {
+    fileprivate var dimmingView: UIView!
+    
+    override var frameOfPresentedViewInContainerView: CGRect {
+        var frame: CGRect = .zero
+        guard let parentContainerView = containerView else { return CGRect() }
+        frame.size = size(forChildContentContainer: presentedViewController,
+                          withParentContainerSize: parentContainerView.bounds.size)
+        frame.origin.y = parentContainerView.frame.height - 355
+        return frame
+    }
+    
+    override init(presentedViewController: UIViewController, presenting presentingViewController: UIViewController?) {
+        super.init(presentedViewController: presentedViewController,
+                   presenting: presentingViewController)
+        DimmingViewSetting()
+    }
+    
+    override func presentationTransitionWillBegin() {
+        containerView?.insertSubview(dimmingView, at: 0)
+        NSLayoutConstraint.activate(
+            NSLayoutConstraint.constraints(withVisualFormat: "V:|[dimmingView]|",
+                                           options: [], metrics: nil, views: ["dimmingView": dimmingView]))
+        NSLayoutConstraint.activate(
+            NSLayoutConstraint.constraints(withVisualFormat: "H:|[dimmingView]|",
+                                           options: [], metrics: nil, views: ["dimmingView": dimmingView]))
+        
+        guard let coordinator = presentedViewController.transitionCoordinator else {
+            dimmingView.alpha = 1.0
+            return
+        }
+        coordinator.animate(alongsideTransition: { _ in
+            self.dimmingView.alpha = 1.0
+        })
+    }
+   
+    override func dismissalTransitionWillBegin() {
+        guard let coordinator = presentedViewController.transitionCoordinator else {
+            dimmingView.alpha = 0.0
+            return
+        }
+        coordinator.animate(alongsideTransition: { _ in
+            self.dimmingView.alpha = 0.0
+        })
+    }
+    
+    override func containerViewWillLayoutSubviews() {
+        presentedView?.frame = frameOfPresentedViewInContainerView
+    }
+    
+    override func size(forChildContentContainer container: UIContentContainer,
+                       withParentContainerSize parentSize: CGSize) -> CGSize {
+        return CGSize(width: parentSize.width, height: 375)
+    }
+}
+
+private extension PresentationController {
+    
+    func DimmingViewSetting() {
+        dimmingView = UIView()
+        dimmingView.translatesAutoresizingMaskIntoConstraints = false
+        dimmingView.backgroundColor = UIColor(white: 0.0, alpha: 0.5)
+        dimmingView.alpha = 0.0
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(dimmingViewTapAction(recognizer:)))
+        dimmingView.addGestureRecognizer(recognizer)
+    }
+    
+    @objc func dimmingViewTapAction(recognizer: UITapGestureRecognizer) {
+        presentingViewController.dismiss(animated: true)
+    }
+}
---
+++ tree/tree/Helper/PresentationManager/PresentationManager.swift
@@ -0,0 +1,24 @@
+//
+//  PresentationManager.swift
+//  tree
+//
+//  Created by Hyeontae on 28/01/2019.
+//  Copyright © 2019 gardener. All rights reserved.
+//
+
+import UIKit
+
+class PresentationManager: NSObject {
+    
+}
+
+extension PresentationManager: UIViewControllerTransitioningDelegate {
+    func presentationController(forPresented presented: UIViewController,
+                                presenting: UIViewController?,
+                                source: UIViewController) -> UIPresentationController? {
+        let presentationController = PresentationController(presentedViewController: presented,
+                                                            presenting: presenting)
+        return presentationController
+    }
+}
+
