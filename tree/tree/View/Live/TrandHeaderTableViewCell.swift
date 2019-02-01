f7ef361ac1cd71c1d62469c26f997bbc8a399115
godpp <taylored@naver.com>
2019-02-01 14:17:08 +0900

    Feat : LIVE TableView Cell
    HeaderCell - Page Title과 국가 선택
    DateHeaderCell - 날짜 (검색어 리스트에 대한 헤더)
    TableView Cell - 검색어 리스트에 대한 Cell

---
+++ tree/tree/View/Live/TrandHeaderTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TrandHeaderTableViewCell.swift
+//  tree
+//
+//  Created by ParkSungJoon on 30/01/2019.
+//  Copyright © 2019 gardener. All rights reserved.
+//
+
+import UIKit
+
+class TrandHeaderTableViewCell: UITableViewCell {
+    @IBOutlet weak var headerView: TrandHeaderView!
+}
---
+++ tree/tree/View/Live/TrandTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TrandTableViewCell.swift
+//  tree
+//
+//  Created by ParkSungJoon on 30/01/2019.
+//  Copyright © 2019 gardener. All rights reserved.
+//
+
+import UIKit
+
+class TrandTableViewCell: UITableViewCell {
+    @IBOutlet weak var listView: TrandListView!
+}
