0fea85f97bbef709a22a55b9aada0c24a47395ac
김혜리 <hyer1k@naver.com>
2019-02-05 02:15:33 +0900

    #49 Feat : Extension picker view

--- tree/tree/Controller/Search/SearchFilterViewController.swift
+++ tree/tree/Controller/Search/SearchFilterViewController.swift
@@ -10,18 +10,30 @@ import UIKit
 
 class SearchFilterViewController: UIViewController {
 
+    @IBOutlet weak var languageStackView: UIStackView!
+    @IBOutlet weak var categoryStackView: UIStackView!
+    @IBOutlet weak var languageLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var selectPickViewer: PickerView!
     @IBOutlet weak var pickerView: UIStackView!
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var keywordSortStackView: UIStackView!
     @IBOutlet weak var keywordSegmentedControl: UISegmentedControl!
     @IBOutlet weak var sortSegmentedControl: UISegmentedControl!
-    
+
     private var selectViewIsPresented: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        settingSegment()
+        registerDelegate()
         roundCorners(layer: saveButton.layer, radius: CGFloat(5))
         roundCorners(layer: self.view.layer, radius: CGFloat(15.0))
+    }
+    
+    private func registerDelegate() {
+        selectPickViewer.delegate = self
+        selectPickViewer.dataSource = self
     }
     
     private func settingSegment() {
@@ -44,15 +56,51 @@ class SearchFilterViewController: UIViewController {
         targetLayer.masksToBounds = true
     }
     
-    @IBAction func selectButtonClick(_ sender: Any) {
+    @IBAction func selectButtonClick(_ sender: UIButton) {
         selectViewIsPresented.toggle()
+        selectPickViewer.tagNumber = sender.tag
+        if selectViewIsPresented {
+            if selectPickViewer.tagNumber == 0 {
+                languageStackView.isHidden = selectViewIsPresented
+                categoryStackView.isHidden = !selectViewIsPresented
+            } else {
+                languageStackView.isHidden = !selectViewIsPresented
+                categoryStackView.isHidden = selectViewIsPresented
+            }
+        } else {
+            languageStackView.isHidden = selectViewIsPresented
+            categoryStackView.isHidden = selectViewIsPresented
+        }
         UIView.animate(withDuration: 0.3) { 
-            self.keywordSortStackView.isHidden = !self.selectViewIsPresented
-            self.pickerView.isHidden = self.selectViewIsPresented
+            self.keywordSortStackView.isHidden = self.selectViewIsPresented
+            self.pickerView.isHidden = !self.selectViewIsPresented
         }
     }
     
     @IBAction func saveButtonClick(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension SearchFilterViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return selectPickViewer.getList().count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectValue = selectPickViewer.getList()[row]
+        if selectPickViewer.tagNumber == 0 {
+            categoryLabel.text = selectValue
+        } else {
+            languageLabel.text = selectValue
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return selectPickViewer.getList()[row]
     }
 }
