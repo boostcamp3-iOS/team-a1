e7aed3b12099b23c0b48260d44a376ded58007e1
onemoon <onemoonstudio@gmail.com>
2019-02-04 22:16:51 +0900

    Feat & Refactor :
    Feat
    Fetch All & fetchCategory Added
    
    Refactor
    Add FetchRequest to remove overlap
    
    - issue : close #51

--- tree/tree/Model/DataManager/ScrapManager.swift
+++ tree/tree/Model/DataManager/ScrapManager.swift
@@ -11,6 +11,16 @@ import CoreData
 import UIKit
 
 final class ScrapManager {
+    static var managedContext: NSManagedObjectContext = {
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return NSManagedObjectContext()
+        }
+        let managedContext: NSManagedObjectContext =
+            appDelegate.persistentContainer.viewContext
+        
+        return managedContext
+    }()
 
     // completion으로 처리를 할 때 -> Void 를 해줘야 한다?
     static func scrapArticle(
@@ -18,16 +28,6 @@ final class ScrapManager {
         category: ArticleCategory,
         imageData: Data?
     ) -> Void {
-        var managedContext: NSManagedObjectContext {
-            guard let appDelegate =
-                UIApplication.shared.delegate as? AppDelegate else {
-                    return NSManagedObjectContext()
-            }
-            let managedContext: NSManagedObjectContext =
-                appDelegate.persistentContainer.viewContext
-
-            return managedContext
-        }
         guard let entity =
             NSEntityDescription.entity(forEntityName: "ScrappedArticle", in: managedContext) else {
                 return
@@ -46,6 +46,57 @@ final class ScrapManager {
         }
     }
     
+    static func fetchAll() -> [ScrappedArticle] {
+        let request: NSFetchRequest<ScrappedArticle> = ScrappedArticle.fetchRequest()
+        let sortDescriptor: NSSortDescriptor =
+            NSSortDescriptor(
+                key: #keyPath(ScrappedArticle.scrappedDate),
+                ascending: false
+        )
+        request.sortDescriptors = [sortDescriptor]
+        var result: [ScrappedArticle] = []
+        result = fetchRequest(request)
+        return result
+    }
+    
+    static func fetchCategory(_ category: ArticleCategory) -> [ScrappedArticle] {
+        var request: NSFetchRequest<ScrappedArticle> = ScrappedArticle.fetchRequest()
+        var result: [ScrappedArticle] = []
+        request.predicate =
+            NSPredicate(
+                format: "%K == %@",
+                #keyPath(ScrappedArticle.category),
+                category.toString()
+        )
+        result = fetchRequest(request)
+        return result
+    }
+    
+    static func removeAllScrappedArticle() {
+        let request: NSFetchRequest<ScrappedArticle> = ScrappedArticle.fetchRequest()
+        var results: [ScrappedArticle] = []
+        do {
+            results = try managedContext.fetch(request)
+            for item in results {
+                managedContext.delete(item)
+            }
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+        print("all data is Removed")
+    }
+    
+    // private 으로 하면 managedContext를 사용할 수 없다.
+    static func fetchRequest(_ request: NSFetchRequest<ScrappedArticle>) -> [ScrappedArticle] {
+        var result: [ScrappedArticle] = []
+        do {
+            result = try managedContext.fetch(request)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+        return result
+    }
     
 }
 
