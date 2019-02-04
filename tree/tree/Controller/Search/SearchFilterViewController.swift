c877522ca41aea9eb319df6e9f80a25aa19d3f42
김혜리 <hyer1k@naver.com>
2019-02-04 18:44:59 +0900

    #49 Feat : Setting default view

--- tree/tree/Controller/Search/SearchFilterViewController.swift
+++ tree/tree/Controller/Search/SearchFilterViewController.swift
@@ -10,10 +10,39 @@ import UIKit
 
 class SearchFilterViewController: UIViewController {
 
+    @IBOutlet weak var keywordSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var sortSegmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        settingSegment()
         roundCorners(layer: self.view.layer, radius: CGFloat(15.0))
+    }
+    
+    private func settingSegment() {
+        keywordSegmentedControl.backgroundColor = .clear
+        sortSegmentedControl.backgroundColor = .clear
+        keywordSegmentedControl.tintColor = .clear
+        sortSegmentedControl.tintColor = .clear
+        
+        let font = UIFont(name: "AppleSDGothicNeo-Bold", size: 16)
+        
+        keywordSegmentedControl.setTitleTextAttributes([
+            NSAttributedString.Key.font: font as Any,
+            NSAttributedString.Key.foregroundColor: UIColor.lightGray
+            ], for: .normal)
+        keywordSegmentedControl.setTitleTextAttributes([
+            NSAttributedString.Key.font: font as Any,
+            NSAttributedString.Key.foregroundColor: UIColor.black
+            ], for: .selected)
+        sortSegmentedControl.setTitleTextAttributes([
+            NSAttributedString.Key.font: font as Any,
+            NSAttributedString.Key.foregroundColor: UIColor.lightGray
+            ], for: .normal)
+        sortSegmentedControl.setTitleTextAttributes([
+            NSAttributedString.Key.font: font as Any,
+            NSAttributedString.Key.foregroundColor: UIColor.black
+            ], for: .selected)
     }
     
     private func roundCorners(layer targetLayer: CALayer, radius withRaidus: CGFloat) {
