1533f17800b3b4a150c7dc7fb65a82b8b957003f
김혜리 <hyer1k@naver.com>
2019-02-08 03:41:05 +0900

    #61 Feat : Create default messageView

---
+++ tree/tree/View/Search/DefautLabelView.swift
@@ -0,0 +1,45 @@
+//
+//  DefautLabelView.swift
+//  tree
+//
+//  Created by hyeri kim on 08/02/2019.
+//  Copyright © 2019 gardener. All rights reserved.
+//
+
+import UIKit
+
+class DefaultLabelView: UIView {
+    
+    @IBOutlet weak var outerView: UIView!
+    @IBOutlet weak var defaultMessage: UILabel!
+    @IBOutlet weak var defaultImage: UIImageView!
+    
+    private let xibName = "DefaultLabelView"
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initXIB()
+        setRadius()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initXIB()
+        setRadius()
+    }
+    
+    private func initXIB() {
+        guard let view = Bundle.main.loadNibNamed(
+            xibName,
+            owner: self,
+            options: nil)?.first as? UIView 
+            else { return }
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
+    
+    private func setRadius() {
+        let radius: CGFloat = 10
+        outerView.roundCorners(layer: outerView.layer, radius: radius)
+    }
+}
