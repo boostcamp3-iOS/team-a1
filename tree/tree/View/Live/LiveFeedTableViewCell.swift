091f604f090e5017cac9b07af120fd9af456d19f
김혜리 <hyer1k@naver.com>
2019-02-09 04:02:31 +0900

    #64 Feat : Create LiveFeed tableview cell

---
+++ tree/tree/View/Live/LiveFeedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LiveFeedTableViewCell.swift
+//  tree
+//
+//  Created by hyeri kim on 09/02/2019.
+//  Copyright © 2019 gardener. All rights reserved.
+//
+
+import UIKit
+
+class LiveFeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
