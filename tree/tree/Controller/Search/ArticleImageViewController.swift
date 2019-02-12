086a5d87bed1cd327b0feb5afc7133c2fcf01f01
김혜리 <hyer1k@naver.com>
2019-02-12 14:13:10 +0900

    #71 Add : Change dismiss position

--- tree/tree/Controller/Search/ArticleImageViewController.swift
+++ tree/tree/Controller/Search/ArticleImageViewController.swift
@@ -44,19 +44,25 @@ class ArticleImageViewController: UIViewController {
     
     @objc private func dismissPanGesture(_ sender: UIPanGestureRecognizer) {
         let touchPosition = sender.location(in: self.view?.window)
-        
         if sender.state == UIGestureRecognizer.State.began {
             initTouchPosition = touchPosition
         } else if sender.state == UIGestureRecognizer.State.changed {
             if touchPosition.y - initTouchPosition.y > 0 {
-                self.view.frame = CGRect(x: 0, y: touchPosition.y - initTouchPosition.y, width: self.view.frame.size.width, height: self.view.frame.size.height)
+                self.view.frame = CGRect(x: 0, 
+                                         y: touchPosition.y - initTouchPosition.y,
+                                         width: self.view.frame.size.width, 
+                                         height: self.view.frame.size.height)
             }
-        } else if sender.state == UIGestureRecognizer.State.ended || sender.state == UIGestureRecognizer.State.cancelled {
-            if touchPosition.y - initTouchPosition.y > 150 {
+        } else if sender.state == UIGestureRecognizer.State.ended ||
+            sender.state == UIGestureRecognizer.State.cancelled {
+            if touchPosition.y - initTouchPosition.y > 200 {
                 self.dismiss(animated: true, completion: nil)
             } else {
                 UIView.animate(withDuration: 0.5, animations: {
-                    self.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height)
+                    self.view.frame = CGRect(x: 0, 
+                                             y: 0, 
+                                             width: self.view.frame.size.width, 
+                                             height: self.view.frame.size.height)
                 })
             }
         }
