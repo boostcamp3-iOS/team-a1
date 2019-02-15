315346e200f0c6ce57e494123e918957e8b47d91
김혜리 <hyer1k@naver.com>
2019-02-15 13:11:07 +0900

    #75 Feat : request web view url

--- tree/tree/View/Search/ArticleWebViewController.swift
+++ tree/tree/View/Search/ArticleWebViewController.swift
@@ -13,17 +13,47 @@ class ArticleWebViewController: UIViewController {
 
     @IBOutlet weak var webView: WKWebView!
     
+    private var loadingView: LoadingView?
+    private var articleURL: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupDelegate()
+        requestURL(url: articleURL)
     }
 
-    func setupDelegate() {
+    private func setupDelegate() {
         webView.uiDelegate = self
         webView.navigationDelegate = self
+    }
+    
+    private func requestURL(url: String) {
+        if let makeUrl = URL(string: url) {
+            let request = URLRequest(url: makeUrl)
+            webView.load(request)
+        }
+    }
+    
+    private func setLoadingView() {
+        let loadingViewFrame = CGRect(
+            x: 0,
+            y: 0, 
+            width: 100,
+            height: 100
+        )
+        loadingView = LoadingView(frame: loadingViewFrame)
+        guard let loadView = loadingView else { return } 
+        loadView.center = self.view.center
+        self.view.addSubview(loadView)        
     }
 }
 
 extension ArticleWebViewController: WKUIDelegate, WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        setLoadingView()
+    }
     
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        self.loadingView?.removeFromSuperview()
+    }
 }
